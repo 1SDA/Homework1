fd93fb47122c864dee61ca96d1b5b0eeda9b2dc5
1SDA <96087031+1SDA@users.noreply.github.com>
2021-12-13 23:58:44 +0200

    Initial commit

---
+++ students.py
@@ -0,0 +1,9 @@
+class Students:
+    def __init__(self):
+        pass
+    def method1():
+        pass
+    def method2():
+        pass
+    def method3():
+        pass
---
+++ test_students.py
@@ -0,0 +1,3 @@
+import pytest
+from students import Students
+
