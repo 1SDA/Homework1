a4de6da8e496ecd3667273c009c77896915363f4
1SDA <96087031+1SDA@users.noreply.github.com>
2021-12-17 20:18:39 +0200

    Reset git environment

--- students.py
+++ students.py
@@ -1,7 +1,7 @@
 
 class Students(object):
     def __init__(self, name, roll, marks):
-        self.name = name
+        self.__name = name
         self.roll = roll
         self.marks = marks
 
@@ -12,4 +12,4 @@ class Students(object):
         return self.roll
 
     def __str__(self):
-        return "Name: " + self.name + " Roll: " + str(self.getroll()) + " Marks: " + str(self.getmarks())
+        return "Name: " + self.__name + " Roll: " + str(self.getroll()) + " Marks: " + str(self.getmarks())
